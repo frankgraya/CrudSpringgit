f2e1e29e0446e6dced7f49e1f032ee4f7d76d672
francisco granados <francisco.granados@tiendasneto.com-at-768971106385>
2024-02-27 00:17:06 -0600

    Se hizo base del proyecto

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,48 +13,5 @@ import java.util.logging.SocketHandler;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
-        int[] numeros = {10, 20, 30, 40, 50};
-
-        // Mostrar los números del arreglo
-        System.out.println("Números en el arreglo:");
-        for (int numero : numeros) {
-
-            System.out.println(numero);
-        }
-
     }
-
-    public static String nombre(String nombre){
-        System.out.println("hola" + 1+nombre);
-
-        return nombre;
-    }
-
-    public static int sumaElementos(int[] arreglo){
-        int suma = 0;
-        for (int elemento : arreglo){
-            suma += elemento;
-        }
-        return suma;
-    }
-
-    public static int multplicarElementos(int[] arreglo){
-        int multiplicacion = 0 ;
-        for (int elemento : arreglo){
-            multiplicacion++;
-            System.out.println("muestra la suma " + multiplicacion);
-            multiplicacion *= elemento;
-        }
-        return multiplicacion;
-    }
-
-    public static void verElementos(int[] arreglo){
-
-        for (int elemento : arreglo) {
-            int i = elemento;
-            System.out.println("su elemento es " + i);
-        }
-    }
-
 }
