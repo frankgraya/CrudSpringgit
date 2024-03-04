37442ad3eabbb3f86b1761d6e529cdbe7405c367
Daniel <daniel_mtz_ojeda@hotmail.com>
2024-03-03 22:40:30 -0600

    Primera relacion 1 a 1

--- src/main/java/com/example/demo/Model/Futbol/Entrenador.java
+++ src/main/java/com/example/demo/Model/Futbol/Entrenador.java
@@ -1,4 +1,32 @@
 package com.example.demo.Model.Futbol;
 
-public class Entrenador {
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "entrenador")
+public class Entrenador
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido_paterno",columnDefinition = "VARCHAR(100)", nullable = false)
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno",columnDefinition = "VARCHAR(100)", nullable = false)
+    private String apellidoMaterno;
+
+    @Column(length = 10, nullable = false)
+    private Integer edad;
+
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
+    private String nacionalidad;
 }
--- src/main/java/com/example/demo/Model/Futbol/Equipo.java
+++ src/main/java/com/example/demo/Model/Futbol/Equipo.java
@@ -1,4 +1,23 @@
 package com.example.demo.Model.Futbol;
+import lombok.*;
+import javax.persistence.*;
 
-public class Equipo {
+@Data // Anotación de Lombok para generar automáticamente getters, setters, toString, equals, hashCode
+@AllArgsConstructor // Genera un constructor con todos los argumentos
+@NoArgsConstructor // Genera un constructor sin argumentos
+@Entity // Indica que esta clase es una entidad JPA
+@Table(name = "equipo") // Especifica el nombre de la tabla en la base de datos
+
+public class Equipo
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Genera automáticamente valores para la clave primaria
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false) // Especifica las propiedades de la columna en la base de datos
+    private String nombre; // Nombre del equipo
+
+    @OneToOne(targetEntity = Entrenador.class) // Establece una relación uno a uno con la clase Entrenador
+    private Entrenador entrenador; // Entrenador del equipo
 }
